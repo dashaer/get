d1018ab64b247862edc9d170e7b693781ea1ef12
dashaer <eremina.da@phystech.edu>
2025-11-12 18:29:04 +0300

    +

--- get-adc/mcp3021_driver.py
+++ get-adc/mcp3021_driver.py
@@ -1,4 +1,5 @@
-import smbus 
+import smbus
+import time 
 
 class MCP3021:
     def __init__(self,dynamic_range,verbose=False):
@@ -11,10 +12,31 @@ class MCP3021:
         self.bus.close()
 
     def get_number(self):
-        data = self.dus.read_word_data(self.address, 0)
+        data = self.bus.read_word_data(self.address, 0)
         lower_data_byte=data >>8
         upper_data_byte = data & 0xFF
-        number = (upper_data_byte << 6) | (lower_data_byte)
+        number = (upper_data_byte << 6) | (lower_data_byte >> 2)
         if self.verbose:
             print(f"принятые данные: {data}, Старший байт: {upper_data_byte:x}, младший байт: {lower_data_byte:x},число:{number}")
-            return number
+        return number
+    def get_voltage(self):
+        n=self.get_number()
+        return (n/1023)*self.dynamic_range
+# *self.dynamic_range
+
+if __name__ == "__main__":
+    try:
+        dac=MCP3021(3.2)
+        while True:
+            try:
+                # dac.set_voltage(voltage)
+                # V=dac.sequential_counting_adc()
+                # print(V)
+                V=dac.get_voltage()
+                print(V)
+                time.sleep(1)
+
+            except ValueError:
+                print("error")
+    finally:
+        dac.deinit()
